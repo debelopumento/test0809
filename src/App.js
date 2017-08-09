94a7b1ecc0b07b89aa6aefbf5052ffe5aac0cc83
debelopumento <imdiye@gmail.com>
2017-08-09 11:25:12 -0700

    added linkedin auth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import LinkedIn from 'react-linkedin-login'
 
 const Home = () =>
   <div>
@@ -11,35 +12,24 @@ const About = () =>
     <h2>About</h2>
   </div>
 
-const Topic = ({ match }) =>
-  <div>
-    <h3>
-      {match.params.topicId}
-    </h3>
-  </div>
-
-const Topics = ({ match }) =>
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.url}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
+class Signin extends Component {
+  callbackLinkedIn = code => {
+    console.log(1, code)
+  }
+  render() {
+    return (
+      <div>
+        <h2>Signin</h2>
+        <LinkedIn
+          clientId="86mqkavfnrp30f"
+          text="LinkedIn Signin"
+          scope={['r_basicprofile', 'r_emailaddress']}
+          callback={this.callbackLinkedIn}
+        />
+      </div>
+    )
+  }
+}
 
 const BasicExample = () =>
   <Router>
@@ -52,7 +42,7 @@ const BasicExample = () =>
           <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to="/topics">Topics</Link>
+          <Link to="/signin">Signin</Link>
         </li>
       </ul>
 
@@ -60,7 +50,7 @@ const BasicExample = () =>
 
       <Route exact path="/" component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
+      <Route path="/signin" component={Signin} />
     </div>
   </Router>
 export default BasicExample
